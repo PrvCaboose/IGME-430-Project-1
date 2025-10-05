446f1405b31730888668f669cce6c331c4a24cfe
John Huggins <92586058+PrvCaboose@users.noreply.github.com>
2025-10-05 11:29:50 -0400

    Implemented remaning api endpoints

--- src/apiResponses.js
+++ src/apiResponses.js
@@ -30,39 +30,73 @@ const getGenres = (request, response) => {
 }
 
 const getBookByGenre = (request, response) => {
-  const genreParams = request.searchParams.getAll('genres');
-  console.log(genreParams)
-  
-  const booksToSend = books.filter(book => {
-    if (book.genres){
-      return genreParams.every(g => book.genres.includes(g));
-    }
-  });
-  console.log(booksToSend);
+  if (request.searchParams['genres']) {
+    // Filter books by genre
+    const genreParams = request.searchParams.getAll('genres');
+
+    const booksToSend = books.filter(book => {
+      if (book.genres){
+        // Only return books in which all search genres are included
+        return genreParams.every(g => book.genres.includes(g));
+      }
+    });
+
+    sendResponse(request, response, 200, booksToSend);
+  } else {
+    sendResponse(request, response, 400, {"message": "Error: Missing genres to filter by"});
+  }
 }
 
 const getAllBooks = (request, response) => {
-    
+  sendResponse(request, response, 200, books);    
 }
 
 const getBooksByAuthor = (request, response) => {
+  if (request.searchParams['author']) { 
+    // Filter books by author
+    const authorParams = request.searchParams['author'];
+
+    const booksToSend = books.filter(book => {
+      return book.author === authorParams;
+    });
+    console.log(booksToSend);
+
+    sendResponse(request, response, 200, booksToSend);
+  } else { // Missing filter params
+    sendResponse(request, response, 400, {"message": "Error: Missing author to filter by"});
+  }
 
 }
 
 const getBooksByTitle = (request, response) => {
+  if (request.searchParams['title']) {
+    // Filter by title
+    const booksToSend = books.filter(book => {
+      return book.title === request.searchParams['title'];
+    });
 
+    sendResponse(request, response, 200, booksToSend);
+  } else {
+    sendResponse(request, response, 400, {'message':"Error: Missing title to filter by"});
+  }
 }
 
 const getFavoriteBooks = (request, response) => {
-    sendResponse(request, response, 200, favoriteBooks);
+  sendResponse(request, response, 200, favoriteBooks);
 }
 
 const markAsFavorite = (request, response) => {
-
+  const book = request.body.book;
+  
+  favoriteBooks.push(book);
+  sendResponse(request, response, 204, {});
 }
 
 const addBook = (request, response) => {
+  const book = request.body.book;
 
+  books.push(book);
+  sendResponse(request, response, 201, {'message': 'Book successfully added'});
 }
 
 module.exports = {
@@ -72,5 +106,6 @@ module.exports = {
     getBooksByAuthor,
     getBooksByTitle,
     addBook,
-    markAsFavorite
+    markAsFavorite,
+    getFavoriteBooks
 }
